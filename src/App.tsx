03bb74d0f8c5999933239a436eab89badad17a5c
atapg <ataparvinghods.com>
2025-03-24 02:39:46 -0400

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import { useEffect, useState } from 'react'
+import reactLogo from '/assets/svg/react.svg'
+import viteLogo from '/assets/svg/vite.svg'
+import './styles/App.css'
+
+function App() {
+	const [count, setCount] = useState(0)
+
+	useEffect(() => {}, [])
+
+	return <>test</>
+}
+
+export default App
